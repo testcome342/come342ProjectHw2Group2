03b7b94ebbf3afeb9ddbe1f5661f0fb244db5a2e
akif095 <akifunal05@gmail.com>
2018-04-13 16:43:53 +0300

    Campaign and Client classes uploaded.

--- src/Agate/domain/Client.java
+++ src/Agate/domain/Client.java
@@ -1,10 +1,37 @@
 package Agate.domain;
-
-
 import java.util.Scanner;
 
 
 
 public class Client {
+    public String companyName;
+    public String companyAddress;
+    public String contactName;
 
+    public void getClientCampaigns() {
+
+    }
+    public void addNewCampaign() {
+
+    }
+    public String getCompanyName() {
+        return companyName;
+
+    }
+    public String getCompanyAddress() {
+        return companyAddress;
+
+    }
+    public String getContactName() {
+        return contactName;
+
+    }
+    public void assignClient(String companyName,String companyAddress,String contactName) {
+
+        this.companyName=companyName;
+        this.companyAddress=companyAddress;
+        this.contactName=contactName;
+
+
+    }
 }
