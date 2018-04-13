ea56d467e4bd0a76a6c71d078bda9f755beaf791
mustafaalibilkay <m.bilkay@windowslive.com>
2018-04-13 21:14:14 +0300

    done

--- src/Agate/control/AddNewCampaign.java
+++ src/Agate/control/AddNewCampaign.java
@@ -1,4 +1,72 @@
 package Agate.control;
 
+import Agate.UI.AddNewCampaignUI;
+import Agate.domain.Campaign;
+import Agate.domain.Client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
 public class AddNewCampaign {
+
+
+
+    private Client model1;
+    private AddNewCampaignUI view;
+    private Campaign model2;
+    public AddNewCampaign(Client model1, AddNewCampaignUI view,Campaign model2) {
+        this.model1 = model1;
+        this.model2=model2;
+        this.view = view;
+        this.view.addClientListener(new ClientListener());
+        this.view.addCampaignListener(new CampaignListener());
+    }
+
+    class ClientListener implements ActionListener{
+
+
+        public void actionPerformed(ActionEvent arg0) {
+            String companyName;
+            String companyAddress;
+            String contactName;
+
+
+            companyName=view.getFirstLine();
+            companyAddress=view.getSecondLine();
+            contactName=view.getThirdLine();
+            model1.assignClient(companyName, companyAddress, contactName);
+            view.setFourthLine(model1.getCompanyName());
+            view.setFifthLine(model1.getCompanyAddress());
+            view.setSixthLine(model1.getContactName());
+
+
+
+        }
+
+    }
+    class CampaignListener implements ActionListener{
+
+
+        public void actionPerformed(ActionEvent arg0) {
+            String title;
+            String campaignStartDate;
+            String campaignFinishDate;
+            String estimatedCost;
+
+            title=view.getFirstLineCampaign();
+            campaignStartDate=view.getSecondLineCampaign();
+            campaignFinishDate=view.getThirdLineCampaign();
+            estimatedCost=view.getFourthLineCampaign();
+            model2.createCampaign(title, campaignStartDate, campaignFinishDate, estimatedCost);
+            view.setTextfieldCampaign1(model2.getTitle());
+            view.setTextfieldCampaign2(model2.getCampaignStartDate());
+            view.setTextfieldCampaign3(model2.getCampaignFinishDate());
+            view.setTextfieldCampaign4(model2.getEstimatedCost());
+        }
+
+    }
+
+
 }
